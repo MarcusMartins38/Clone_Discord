8136205aebe6902e02b57785d77730ad8f01ace9
Marcus Martins <marcusmartins38@gmail.com>
2020-06-23 15:34:23 -0400

    Creating Components: ServerList, ServerButton

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Container, Separator } from "./styles";
+
+import ServerButton from "../ServerButton";
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={10} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton mentions={43} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+    </Container>
+  );
+};
+
+export default ServerList;
