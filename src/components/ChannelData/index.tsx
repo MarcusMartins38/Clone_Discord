548376b2cffb28e5f9cf7863a3a1c9d69d6b3151
Marcus Martins <marcusmartins38@gmail.com>
2020-06-24 00:17:48 -0400

    Component ChannelMessage

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,40 @@
 import React from "react";
 
+import ChannelMessage, { Mention } from "../ChannelMessage";
 import { Container, Messages, InputWrapper, Input, InputIcon } from "./styles";
 
 const ChannelData: React.FC = () => {
   return (
     <Container>
-      <Messages />
+      <Messages>
+        <ChannelMessage
+          author="Marcus Martins"
+          date="23/06/2020"
+          content="Hoje é o meu aniversário!"
+        />
+
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="John Doe"
+            date="23/06/2020"
+            content="Just Repeating the same!"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Diego Fernandes"
+          date="23/06/2020"
+          content={
+            <>
+              <Mention>@Marcus Martins</Mention>, me carrega no LoL e CS de novo
+              por favor?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversarem #chat-livre" />
